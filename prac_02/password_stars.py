b1a5ccd1963c26ac1af0549bac61813da18b9347
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-05-27 16:53:34 +0800

    Add password check program

---
+++ prac_02/password_stars.py
@@ -0,0 +1,6 @@
+MINIMUM_LENGTH = 8
+password = input("Enter password: ")
+while len(password) < MINIMUM_LENGTH:
+    print("Invalid password")
+    password = input("Enter password: ")
+print("*" * len(password))
