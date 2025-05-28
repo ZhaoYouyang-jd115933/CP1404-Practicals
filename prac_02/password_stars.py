ee1431706bd661a27aaf5b1410df49b625d37c99
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-05-28 15:30:51 +0800

    Add algorithm for the password check function

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,9 +1,27 @@
+"""
+MINIMUM_LENGTH = 8
+function main()
+    password = get_valid_password()
+    print_asterisks(password)
+
+function get_valid_password()
+    get password
+    while length of password less than MINIMUM_LENGTH
+          display "Invalid password"
+          get password
+    return password
+
+function print_asterisks(password)
+    display "*" * length of password
+"""
 MINIMUM_LENGTH = 8
 def main():
+    """Get valid password and print asterisks"""
     password = get_valid_password()
     print_asterisks(password)
 
 def get_valid_password():
+    """Get valid password"""
     password = input("Enter password: ")
     while len(password) < MINIMUM_LENGTH:
         print("Invalid password")
@@ -11,5 +29,6 @@ def get_valid_password():
     return password
 
 def print_asterisks(password):
+    """Print asterisks based on length of password"""
     print("*" * len(password))
 main()
