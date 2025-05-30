7cfbe0a2e55a343c00db95933696845ed8448e96
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-05-30 21:06:39 +0800

    Add score determination program

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,6 +2,11 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+EXCELLENT_THRESHOLD = 90
+PASS_THRESHOLD = 50
+
 get score
 while score less than 0 or score greater than 100
     display "Invalid score"
@@ -16,13 +21,17 @@ else
 
 # TODO: Fix this!
 
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+EXCELLENT_THRESHOLD = 90
+PASS_THRESHOLD = 50
 score = float(input("Enter score: "))
-while score < 0 or score > 100:
+while score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
     print("Invalid score")
     score = float(input("Enter score: "))
-if score > 50:
-    print("Passable")
-elif score > 90:
+if score > EXCELLENT_THRESHOLD:
     print("Excellent")
+elif score > PASS_THRESHOLD:
+    print("Passable")
 else:
     print("Bad")
