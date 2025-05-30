3f6c34cf26d8a0fd595e5f29350afdf22121e53b
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-05-30 22:34:25 +0800

    Get valid score

--- prac_02/score.py
+++ prac_02/score.py
@@ -7,31 +7,40 @@ MAXIMUM_SCORE = 100
 EXCELLENT_THRESHOLD = 90
 PASS_THRESHOLD = 50
 
-get score
-while score less than MINIMUM_SCORE or score greater than MAXIMUM_SCORE
-    display "Invalid score"
-    get score
-if score > EXCELLENT_THRESHOLD
-    display "Excellent"
-else if score > PASS_THRESHOLD
-    display "Passable"
-else
-    display "Bad"
-"""
+function main()
+    score = validate_score()
+    if score > EXCELLENT_THRESHOLD
+        display "Excellent"
+    else if score > PASS_THRESHOLD
+        display "Passable"
+    else
+        display "Bad"
 
-# TODO: Fix this!
+function validate_score()
+    score = float(input("Enter score: "))
+    while score < MINIMUM_SCORE or score > MAXIMUM_SCORE
+        display "Invalid score"
+        score = float(input("Enter score: "))
+    return score
+"""
 
 MINIMUM_SCORE = 0
 MAXIMUM_SCORE = 100
 EXCELLENT_THRESHOLD = 90
 PASS_THRESHOLD = 50
-score = float(input("Enter score: "))
-while score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
-    print("Invalid score")
+def main():
+    score = validate_score()
+    if score > EXCELLENT_THRESHOLD:
+        print("Excellent")
+    elif score > PASS_THRESHOLD:
+        print("Passable")
+    else:
+        print("Bad")
+
+def validate_score():
     score = float(input("Enter score: "))
-if score > EXCELLENT_THRESHOLD:
-    print("Excellent")
-elif score > PASS_THRESHOLD:
-    print("Passable")
-else:
-    print("Bad")
+    while score < MINIMUM_SCORE or score > MAXIMUM_SCORE:
+        print("Invalid score")
+        score = float(input("Enter score: "))
+    return score
+main()
