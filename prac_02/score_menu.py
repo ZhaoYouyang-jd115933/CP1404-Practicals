86d1e9dd3dff00fc117a71abb5c33ced18dbf394
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-05-31 01:07:14 +0800

    Print menu

---
+++ prac_02/score_menu.py
@@ -0,0 +1,9 @@
+MINIMUM_SCORE = 0
+MAXIMUM_SCORE = 100
+EXCELLENT_THRESHOLD = 90
+PASS_THRESHOLD = 50
+
+MENU = "(G)et a valid score\n(P)rint result\n(S)how stars\n(Q)uit"
+
+def main():
+    print(MENU)
