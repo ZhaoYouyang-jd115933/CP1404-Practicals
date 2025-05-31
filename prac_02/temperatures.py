152b3fa8164978ba22753c052cd0fa4ba806ac02
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-01 00:20:18 +0800

    Using function to convert Fahrenheit to Celsius

--- prac_02/temperatures.py
+++ prac_02/temperatures.py
@@ -16,7 +16,7 @@ function main()
              display Fahrenheit
          else if choice equal to "F"
              get Fahrenheit
-             Celsius = 5 / 9 * (Fahrenheit - 32)
+             Celsius = convert_Fahrenheit_to_Celsius(Fahrenheit)
              display Celsius
          else
              display "Invalid option"
@@ -24,8 +24,11 @@ function main()
          get choice
     display "Thank you."
 
-function convert_celsius_to_fahrenheit(Celsius)
+function convert_Celsius_to_Fahrenheit(Celsius)
     return Celsius * 9.0 / 5 + 32
+
+function convert_Fahrenheit_to_Celsius(Fahrenheit)
+    return 5 / 9 * (Fahrenheit - 32)
 """
 
 MENU = """C - Convert Celsius to Fahrenheit
@@ -43,7 +46,7 @@ def main():
             print(f"Result: {fahrenheit:.2f} F")
         elif choice == "F":
             fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
+            celsius = convert_fahrenheit_to_celsius(fahrenheit)
             print(f"Result: {celsius:.2f} C")
         else:
             print("Invalid option")
@@ -54,4 +57,8 @@ def main():
 def convert_celsius_to_fahrenheit(celsius):
     """Convert celsius to fahrenheit"""
     return celsius * 9.0 / 5 + 32
+
+def convert_fahrenheit_to_celsius(fahrenheit):
+    """Convert fahrenheit to celsius"""
+    return 5 / 9 * (fahrenheit - 32)
 main()
