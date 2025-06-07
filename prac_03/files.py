52216d3f07963f9e2569c44dffdcd6679dc1d059
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-07 17:58:56 +0800

    Get name

---
+++ prac_03/files.py
@@ -0,0 +1,4 @@
+# 1.Write code that asks the user for their name, then opens a file called name.txt and writes that name to it.
+# Use open and close for this question.
+FILENAME = "name.txt"
+name = input("Enter name: ")
