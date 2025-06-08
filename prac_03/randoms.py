cfd8cae86257b5b5cbe982a09636c465cdf88827
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-08 17:13:56 +0800

    Answer the questions in randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,28 @@
+import random
+
+print(random.randint(5, 20))  # line 1
+print(random.randrange(3, 10, 2))  # line 2
+print(random.uniform(2.5, 5.5))  # line 3
+
+# What did you see on line 1?
+# It prints all the integers between 5 and 20
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is 5 and the largest number is 20
+
+# What did you see on line 2?
+# The result of this random function is 3, 5, 7, 9
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is 3 and the largest number is 9
+# Could line 2 have produced a 4?
+# Line 2 could not have produced a 4. This is because step is 2 and start from 3, so it only print odd numbers
+
+# What did you see on line 3?
+# It prints any float between 2.5 and 5.5, such as 2.7785295157992067, 3.5186683110679136
+# What was the smallest number you could have seen, what was the largest?
+# The smallest number is close to 2.5, but it can not be exactly 2.5
+# The largest number is close to 5.5, but it can not be exactly 5.5
+
+
+
+
+
