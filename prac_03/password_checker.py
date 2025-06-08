dd6ff04f6b86f45a5f9a4b48eb26336fef6aa678
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-08 16:06:46 +0800

    Accumulate special characters

--- prac_03/password_checker.py
+++ prac_03/password_checker.py
@@ -53,7 +53,11 @@ def is_valid_password(password):
     # TODO: if special characters are required, then check the count of those
     # and return False if it's zero
     if IS_SPECIAL_CHARACTER_REQUIRED:
-
+        for character in password:
+            if character in SPECIAL_CHARACTERS:
+                number_of_special += 1
+        if number_of_special == 0:
+            return False
 
     # if we get here (without returning False), then the password must be valid
     return True
