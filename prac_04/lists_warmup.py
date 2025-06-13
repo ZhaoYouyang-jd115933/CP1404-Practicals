d71155981ee1b504193da6a6730b1bc7202ddaa5
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-13 21:19:16 +0800

    Change the last element to 1

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -11,3 +11,6 @@ numbers = [3, 1, 4, 1, 5, 9, 2]
 
 # 1.Change the first element of numbers to "ten" (the string, not the number 10)
 numbers[0] = "ten"
+# 2.Change the last element of numbers to 1
+numbers[-1] = 1
+print(numbers)
