d7a5c6962d5b80ed37111b98afc970b50c820b6a
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-13 21:27:23 +0800

    Print whether 9 is in numbers

--- prac_04/lists_warmup.py
+++ prac_04/lists_warmup.py
@@ -16,3 +16,5 @@ numbers[-1] = 1
 print(numbers)
 # 3.Print all the elements from numbers except the first two (slice)
 print(numbers[2:])
+# 4.Print whether 9 is an element of numbers
+print(9 in numbers)
