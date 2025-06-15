023f1b00262a641378e67ee3fdbf73c3698495d8
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-15 18:28:30 +0800

    Check length of numbers less than 6

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -3,3 +3,6 @@ MAXIMUM_NUMBER = 45
 NUMBER_OF_EACH_LINE = 6
 
 number_of_quick_picks = int(input("How many quick picks? "))
+for number in range(number_of_quick_picks):
+    numbers = []
+    while len(numbers) < NUMBER_OF_EACH_LINE:
