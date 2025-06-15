e02e7275a94d4360029e435f118e696bc69c6ac0
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-15 15:16:09 +0800

    Prompt user enter username

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -9,3 +9,7 @@ print(f"The last number is {numbers[-1]}")
 print(f"The smallest number is {min(numbers)}")
 print(f"The largest number is {max(numbers)}")
 print(f"The average of the numbers is {average_number}")
+
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
+             'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+username = input("Enter username: ")
