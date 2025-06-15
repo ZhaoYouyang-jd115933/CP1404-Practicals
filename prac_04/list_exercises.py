cad07231d23475346c05fa96d6bbb8b58338a688
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-15 15:21:57 +0800

    Determine whether username in usernames

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -13,3 +13,7 @@ print(f"The average of the numbers is {average_number}")
 usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn',
              'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
 username = input("Enter username: ")
+if username in usernames:
+    print("Access granted")
+else:
+    print("Access denied")
