1b9016e0aac8271b3af414fbf023254e17fa69f9
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-15 15:05:16 +0800

    Print the fist number in a list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -4,3 +4,4 @@ for number in range(NUMBER_OF_INPUTS):
     enter_number = int(input("Number: "))
     numbers.append(enter_number)
 average_number = sum(numbers) / len(numbers)
+print(f"The first number is {numbers[0]}")
