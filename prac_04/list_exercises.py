e4d6ac27b192aec33c3d798156566a670d1c04eb
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-15 15:09:12 +0800

    Print the smallest number in a list

--- prac_04/list_exercises.py
+++ prac_04/list_exercises.py
@@ -6,3 +6,4 @@ for number in range(NUMBER_OF_INPUTS):
 average_number = sum(numbers) / len(numbers)
 print(f"The first number is {numbers[0]}")
 print(f"The last number is {numbers[-1]}")
+print(f"The smallest number is {min(numbers)}")
