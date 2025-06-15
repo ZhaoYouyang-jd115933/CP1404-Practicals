47ff8d48638a130961038a88b17423e01626d91e
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-15 18:30:25 +0800

    Get random number between 1 and 45

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,3 +1,4 @@
+import random
 MINIMUM_NUMBER = 1
 MAXIMUM_NUMBER = 45
 NUMBER_OF_EACH_LINE = 6
@@ -6,3 +7,4 @@ number_of_quick_picks = int(input("How many quick picks? "))
 for number in range(number_of_quick_picks):
     numbers = []
     while len(numbers) < NUMBER_OF_EACH_LINE:
+        random_number = random.randint(MINIMUM_NUMBER, MAXIMUM_NUMBER)
