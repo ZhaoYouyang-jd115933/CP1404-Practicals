a281dc19a431c326a072cb5d95bd0e9e24099c1b
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-20 00:29:02 +0800

    Print all the states and names neatly lined up

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -7,7 +7,9 @@ File needs reformatting
 # TODO: Reformat this file so the dictionary code follows PEP 8 convention
 CODE_TO_NAME = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                 "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania", "SA": "South Australia"}
-print(CODE_TO_NAME)
+
+for code in CODE_TO_NAME:
+    print(f"{code:3} is {CODE_TO_NAME[code]}")
 
 state_code = input("Enter short state: ").upper()
 while state_code != "":
