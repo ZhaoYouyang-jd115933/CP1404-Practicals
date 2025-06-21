9da6ed01f98ea0fd853063d677f6c58151c158b4
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-22 02:18:23 +0800

    Add a dictionary to count word occurrences

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,3 +1,9 @@
 text = input("Text: ")
 words = text.split()
+words_to_count = {}
+for word in words:
+    if word in words_to_count:
+        words_to_count[word] += 1
+    else:
+        words_to_count[word] = 1
 
