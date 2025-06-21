5372b5f6db2518bc615a8fb66ac5816989b21032
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-22 02:28:07 +0800

    Add printing words and counts in alphabetical order

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -6,4 +6,6 @@ for word in words:
         words_to_count[word] += 1
     else:
         words_to_count[word] = 1
-
+max_length = max(len(word) for word in words_to_count)
+for word in sorted(words_to_count):
+    print(f"{word:10} : {words_to_count[word]}")
