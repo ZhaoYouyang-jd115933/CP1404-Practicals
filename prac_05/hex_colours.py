74b578f402a03922b71d11ae944fc4a14ed4cc52
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-21 20:29:14 +0800

    Add for loop to print colour name and code

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -2,5 +2,8 @@ COLOURS = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f
            "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc", "AntiqueWhite": "#faebd7",
            "Apricot": "#fbceb1", "Aqua": "#00ffff"}
 
+for name in COLOURS:
+    print(f"{name:16} is {COLOURS[name]}")
+
 
 
