367d51685e2adcfd6a83ea4e2246ac12123dcaad
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-22 03:00:53 +0800

    Refactor dictionary name COLOURS to COLOUR_TO_CODE

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -1,14 +1,14 @@
-COLOURS = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff", "Alizarin crimson": "#e32636",
+COLOUR_TO_CODE = {"Absolute Zero": "#0048ba", "Acid Green": "#b0bf1a", "AliceBlue": "#f0f8ff", "Alizarin crimson": "#e32636",
            "Amaranth": "#e52b50", "Amber": "#ffbf00", "Amethyst": "#9966cc", "AntiqueWhite": "#faebd7",
            "Apricot": "#fbceb1", "Aqua": "#00ffff"}
 
-for name in COLOURS:
-    print(f"{name:16} is {COLOURS[name]}")
+for name in COLOUR_TO_CODE:
+    print(f"{name:16} is {COLOUR_TO_CODE[name]}")
 
 colour_name = input("Enter short state: ").title()
 while colour_name != "":
     try:
-        print(colour_name, "is", COLOURS[colour_name])
+        print(colour_name, "is", COLOUR_TO_CODE[colour_name])
     except KeyError:
         print("Invalid short state")
     colour_name = input("Enter short state: ").title()
