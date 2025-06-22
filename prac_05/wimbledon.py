0f99ada938f9135e36cdde9e1773edf0fbb19fe5
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-06-22 16:44:42 +0800

    Refactor the program that counts the number of times each champion has won into a function

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -2,6 +2,9 @@ FILENAME = "wimbledon.csv"
 
 def main():
     records = load_data()
+    champions_to_count = count_champions(records)
+    print(champions_to_count)
+
 def load_data():
     """Convert data in file into a list of lists"""
     records = []
@@ -12,6 +15,18 @@ def load_data():
             records.append(data_parts)
     return records
 
+def count_champions(records):
+    """Extract champion name and count how many times they have won."""
+    champions_to_count = {}
+    for data_parts in records:
+        champion = data_parts[2]
+        if champion in champions_to_count:
+            champions_to_count[champion] += 1
+        else:
+            champions_to_count[champion] = 1
+    return champions_to_count
+
+main()
 
 
 
