5fd7bc7ef2fa493b9007978c350c90756951f5c7
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-07-06 00:10:45 +0800

    Update code to print formatted three guitars

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -19,3 +19,7 @@ while name != "":
     print()
 guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+print("These are my guitars:")
+for number, guitar in enumerate(guitars, 1):
+    vintage_string = "(vintage)" if guitar.is_vintage() else ""
+    print(f"Guitar {number}: {guitar.name:>20} ({guitar.year}), worth ${guitar.cost:10,.2f}{vintage_string}")
