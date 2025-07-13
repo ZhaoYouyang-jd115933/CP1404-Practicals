8c26f5ebb8cf9508cee39e12cd05490f8ef64bab
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-07-13 13:56:39 +0800

    Refactor code to return a list of Guitar object into function

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -3,6 +3,7 @@ from prac_07.guitar import Guitar
 
 def main():
     add_guitars()
+    guitars = read_file()
 
 def add_guitars():
     """Put all new guitars in a list based on the user input"""
@@ -15,6 +16,7 @@ def add_guitars():
         print(f"{name} ({year}) : {cost} added.")
         save_new_guitars(new_guitars)
         name = input("Name: ")
+    return new_guitars
 
 def save_new_guitars(new_guitars):
     """Add the input new guitars in csv file"""
@@ -22,4 +24,14 @@ def save_new_guitars(new_guitars):
         writer = csv.writer(out_file)
         writer.writerows(new_guitars)
 
+def read_file():
+    """Read guitars from guitars.csv and return a list of Guitar objects"""
+    guitars = []
+    in_file = open('guitars.csv', 'r')
+    for line in in_file:
+        parts = line.strip().split(",")
+        guitar = Guitar(parts[0], int(parts[1]), parts[2])
+        guitars.append(guitar)
+    in_file.close()
+    return guitars
 main()
