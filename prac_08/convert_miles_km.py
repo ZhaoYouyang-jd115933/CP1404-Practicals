78ea02e3df64a9451ccba45c7e19eae5b400d4e3
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-07-20 13:15:56 +0800

    Add handling calculation and increment function and validate the user input

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -15,5 +15,25 @@ class ConvertMilesApp(App):
         self.convert_result = "0.0"
         return self.root
 
+    def handle_calculate(self):
+        """Calculate kilometers from the input miles and update the result label."""
+        number = self.get_valid_miles()
+        km_result = number * MILES_TO_KM
+        self.root.ids.convert_result.text = str(km_result)
+
+    def handle_increment(self, change):
+        """Handler for pressing up or down button."""
+        new_number = self.get_valid_miles() + change
+        self.root.ids.input_number.text = str(new_number)
+        self.handle_calculate()
+
+    def get_valid_miles(self):
+        """Validate miles input and convert it to float."""
+        try:
+            input_mile = float(self.root.ids.input_number.text)
+            return input_mile
+        except ValueError:
+            return 0
+
 ConvertMilesApp().run()
 
