08cc275d0fa9a6f9682bb9df06b0a36b1f212cd8
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-07-27 16:33:24 +0800

    Update methods to add musicians and play their instruments in Band class

--- prac_09/band.py
+++ prac_09/band.py
@@ -7,3 +7,11 @@ class Band:
     def __str__(self):
         """Return a string representation of the band, including the name and the list of musicians."""
         return f"{self.name} ({', '.join(str(musician) for musician in self.musicians)})"
+
+    def add(self, musician):
+        """Add a musician to band"""
+        self.musicians.append(musician)
+
+    def play(self):
+        """Return a string of all musicians playing their instruments."""
+        return "\n".join(musician.play() for musician in self.musicians)
