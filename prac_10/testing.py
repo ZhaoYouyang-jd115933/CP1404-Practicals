ed53b3a6d099a7efb0e62e586d77b86651c3ec5d
Zhao Youyang <youyang.zhao@my.jcu.edu.au>
2025-08-02 20:31:40 +0800

    Fix repeat_string function and add two assert statements to check fuel

--- prac_10/testing.py
+++ prac_10/testing.py
@@ -9,7 +9,7 @@ from prac_06.car import Car
 
 def repeat_string(s, n):
     """Repeat string s, n times, with spaces in between."""
-    return s * n
+    return " ".join([s] * n)
 
 
 def is_long_word(word, length=5):
@@ -38,14 +38,18 @@ def run_tests():
     # assert test with custom message,
     # used to see if Car's init method sets the odometer correctly
     # this should pass (no output)
-    car = Car()
+    car = Car("Test Car")
     assert car._odometer == 0, "Car does not set odometer correctly"
+    assert car.fuel == 0, "Car does not set fuel to default correctly"
+
+
 
     # TODO: 2. write assert statements to show if Car sets the fuel correctly
     # Note that Car's __init__ function sets the fuel in one of two ways:
     # using the value passed in or the default
     # You should test both of these
-    car = Car(fuel=10)
+    car = Car("Test Car", fuel=10)
+    assert car.fuel == 10, "Car does not set fuel correctly"
 
 
 run_tests()
